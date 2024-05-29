2748aa904903158cd71d82c543e21a443f03ec31
Sandip <121609887+sandipbera1001@users.noreply.github.com>
2024-05-29 17:15:23 +0530

    Update qrcode.go

--- qrcode.go
+++ qrcode.go
@@ -60,8 +60,8 @@ import (
 	"log"
 	"os"
 
-	bitset "github.com/skip2/go-qrcode/bitset"
-	reedsolomon "github.com/skip2/go-qrcode/reedsolomon"
+	bitset "github.com/sandipbera1001/go-qrcode/bitset"
+	reedsolomon "github.com/sandipbera1001/go-qrcode/reedsolomon"
 )
 
 // Encode a QR Code and return a raw PNG image.
